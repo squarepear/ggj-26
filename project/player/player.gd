6d9c7d37d28d1875bf690b630f3cb58eb1b5ab10
Pandora <pandorar9000@gmail.com>
2026-01-31 16:03:28 -0500

    Give player death state

--- project/player/player.gd
+++ project/player/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
+signal killed
+
 @onready var _input_component: InputComponent = $InputComponent
 @onready var _movement_compenent: MovementCompenent = $MovementCompenent
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var _hitbox_component: HitboxComponent = $HitboxComponent
 @onready var _vision_blocker: VisionBlocker = $VisionBlocker
 
-
+var can_move := true
 
 func _ready():
 	%Head.play("default")
@@ -14,10 +16,10 @@ func _ready():
 
 
 func _physics_process(_delta: float) -> void:
-	_input_component.apply_inputs()
-
-	_movement_compenent.set_dir(_input_component.dir)
-	_movement_compenent.update()
+	if can_move:
+		_input_component.apply_inputs()
+		_movement_compenent.set_dir(_input_component.dir)
+		_movement_compenent.update()
 
 
 func _on_hitbox_component_hit(hurtbox: HurtboxComponent) -> void:
@@ -37,4 +39,5 @@ func _on_blink_timer_timeout() -> void:
 
 
 func _on_vision_blocker_fully_splotted() -> void:
-	print("You died")
+	can_move = false
+	killed.emit()
--- project/world/world.gd
+++ project/world/world.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var _wave_component: WaveComponent = %WaveComponent
 @onready var _wave_generator: WaveGenerator = %WaveGenerator
 
+var player_living := true
 
 func _ready() -> void:
 	_wave_component.wave_completed.connect(next_wave)
@@ -10,11 +11,16 @@ func _ready() -> void:
 
 
 func next_wave() -> void:
-	_wave_generator.generate()
-	_wave_component.start()
-	_display_wave_label()
+	if player_living:
+		_wave_generator.generate()
+		_wave_component.start()
+		_display_wave_label()
 
 
 func _display_wave_label() -> void:
 	%WaveLabel.text = "Wave " + str(_wave_generator.get_current_wave())
 	%AnimationPlayer.play("wave_announcement")
+
+
+func _on_player_killed() -> void:
+	player_living = false
