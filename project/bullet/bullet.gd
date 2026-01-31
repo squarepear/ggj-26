077e41e7b8da202ba0e9c91bdf2d02c641c166a7
jadicodes <jadilmiller@gmail.com>
2026-01-31 12:46:35 -0500

    Emit spawned and job_completed signals from spawner

--- project/bullet/bullet.gd
+++ project/bullet/bullet.gd
@@ -1,4 +1,7 @@
+class_name Bullet
 extends CharacterBody2D
+
+signal screen_exited
 
 @onready var _movement_compenent: MovementCompenent = $MovementCompenent
 @onready var _target_component: TargetComponent = $TargetComponent
@@ -8,8 +11,14 @@ func initialize():
 	_movement_compenent.set_dir(_target_component.get_target_direction())
 	_movement_compenent.update()
 
+
 func _physics_process(delta: float) -> void:
 	_movement_compenent.update()
 
-func set_target(target):
+
+func set_target(target) -> void:
 	_target_component.set_target(target)
+
+
+func _on_visible_on_screen_notifier_screen_exited() -> void:
+	screen_exited.emit()
--- project/component/spawner_component.gd
+++ project/component/spawner_component.gd
@@ -1,12 +1,20 @@
 class_name SpawnerComponent
 extends Node2D
 
+signal spawned
+signal job_completed
+
 @export var bullet : PackedScene
 
 
 func spawn(target):
-	var bullet_instance = bullet.instantiate()
+	var bullet_instance: Bullet = bullet.instantiate()
 	add_child(bullet_instance)
+	bullet_instance.screen_exited.connect(_on_bullet_screen_exited)
 	bullet_instance.set_target(target)
 	bullet_instance.initialize()
-	
+	spawned.emit()
+
+
+func _on_bullet_screen_exited() -> void:
+	job_completed.emit()
