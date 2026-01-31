0d3050779d2701d83027c960d43161edfb259f9c
jadicodes <jadilmiller@gmail.com>
2026-01-30 21:45:15 -0500

    Add blinking animations

--- project/player/player.gd
+++ project/player/player.gd
@@ -5,6 +5,11 @@ extends CharacterBody2D
 @onready var health_component: HealthComponent = $HealthComponent
 
 
+
+func _ready():
+	%Head.play("default")
+
+
 func _physics_process(_delta: float) -> void:
 	_input_component.apply_inputs()
 
@@ -12,6 +17,19 @@ func _physics_process(_delta: float) -> void:
 	_movement_compenent.update()
 
 
+<<<<<<< Updated upstream
 
 func _on_hitbox_component_hit(hurtbox: HurtboxComponent) -> void:
 	health_component.hurt()
+=======
+func _blink():
+	var random_int: int = randi_range(0,1)
+	if random_int == 0:
+		%Head.play("blink1")
+	else:
+		%Head.play("blink2")
+
+
+func _on_blink_timer_timeout() -> void:
+	_blink()
+>>>>>>> Stashed changes
