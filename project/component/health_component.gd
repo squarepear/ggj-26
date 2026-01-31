72ba289edf416bca5bc469c4f89038daff57f2f8
Pandora <pandorar9000@gmail.com>
2026-01-30 21:38:11 -0500

    Damage player

--- project/component/health_component.gd
+++ project/component/health_component.gd
@@ -6,8 +6,9 @@ signal on_health_zero
 var _health := 10
 
 
-func hurt(damage: int) -> void:
+func hurt(damage = 1) -> void:
 	_health -= damage
+	print(_health)
 	if _health <= 0:
 		on_health_zero.emit()
 
--- project/player/player.gd
+++ project/player/player.gd
@@ -2,9 +2,16 @@ extends CharacterBody2D
 
 @onready var _input_component: InputComponent = $InputComponent
 @onready var _movement_compenent: MovementCompenent = $MovementCompenent
+@onready var health_component: HealthComponent = $HealthComponent
+
 
 func _physics_process(_delta: float) -> void:
 	_input_component.apply_inputs()
 
 	_movement_compenent.set_dir(_input_component.dir)
 	_movement_compenent.update()
+
+
+
+func _on_hitbox_component_hit(hurtbox: HurtboxComponent) -> void:
+	health_component.hurt()
