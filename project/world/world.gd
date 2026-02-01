de32717ad584a90a332abb8f854544d940221520
Pandora <pandorar9000@gmail.com>
2026-02-01 13:28:03 -0500

    Add slow down effect to game over music

--- project/world/world.gd
+++ project/world/world.gd
@@ -30,9 +30,17 @@ func _display_wave_label() -> void:
 
 func _on_player_killed() -> void:
 	if !_game_over:
+		
+		var tween = get_tree().create_tween()
+		tween.tween_property(_music, "pitch_scale", 0.1, 5.0)
+		tween.finished.connect(_on_tween_finished)
+		
 		_death_sound.play()
 		player_living = false
 		_game_over = true
 		var _game_over = GAME_OVER.instantiate()
 		%CanvasLayer.add_child(_game_over)
 		_game_over.update_waves_label(_wave_generator.get_current_wave())
+
+func _on_tween_finished() -> void:
+	_music.stop()
